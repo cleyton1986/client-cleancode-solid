8fc6b54de1af5d79bb649c34a2df747c720e8de1
cleyton.alves <cleyton.alves@ferreiracosta.com.br>
2024-05-14 20:07:08 -0300

    :recycle: refactor: shift to clean architecture

--- internal/usecases/user.go
+++ internal/usecases/user.go
@@ -5,28 +5,28 @@ import (
 	"github.com/cleyton1986/client-cleancode-solid/internal/interfaces/repositories"
 )
 
-type UserRepository = *repositories.UserRepository  // Mudança aqui para aceitar ponteiro
+type UserRepository = *repositories.UserRepository
 
 type UserUseCase struct {
-    repo UserRepository
+	repo UserRepository
 }
 
 func NewUserUseCase(repo UserRepository) *UserUseCase {
-    return &UserUseCase{repo: repo}
+	return &UserUseCase{repo: repo}
 }
 
 func (uc *UserUseCase) CreateUser(user *entities.User) error {
-    return uc.repo.Create(user)
+	return uc.repo.Create(user)
 }
 
 func (uc *UserUseCase) ListUsers() ([]entities.User, error) {
-    return uc.repo.FindAll()
+	return uc.repo.FindAll()
 }
 
 func (uc *UserUseCase) UpdateUser(user *entities.User) error {
-    return uc.repo.Update(user)
+	return uc.repo.Update(user)
 }
 
 func (uc *UserUseCase) DeleteUser(id uint) error {
-    return uc.repo.Delete(id)
+	return uc.repo.Delete(id)
 }
